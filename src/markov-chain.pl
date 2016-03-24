a5a2697bfb97b919b1401f75ad35b2c67a3d278c
Andrew Gray <adsgray@gmail.com>
2016-03-24 09:09:08 -0700

    commit ongoing improvements -- it actually works now

--- src/markov-chain.pl
+++ src/markov-chain.pl
@@ -1,13 +1,9 @@
 #!/usr/bin/perl
 
-
-
-my %map = ();
-
 # read in a text file line by line
 # then generate a table of
 # word pair -> next_word mappings
-# with frequencies
+# with frequencies TODO: use frequencies
 
 sub dbg_array {
     my $aref = shift;
@@ -15,6 +11,24 @@ sub dbg_array {
         print "$ent ";
     }
     print "\n";
+}
+
+sub dbg_map {
+    my $mref = shift;
+    foreach my $k (keys %$mref) {
+        print "$k -> " . $mref->{$k} . "\n";
+    }
+}
+
+sub dump_map {
+    my $mref = shift;
+    foreach my $k (sort keys %$mref) {
+        my $submap = $mref->{$k};
+        my $next = join ",", keys %$submap;
+        # TODO: print out frequencies
+        # TODO: print out in json format
+        print "$k -> $next\n";
+    }
 }
 
 # return true if line should be skipped
@@ -38,39 +52,112 @@ sub exclude_line {
         return 1;
     }
 
+    if ($line =~ /^\s*$/) {
+        return 1;
+    }
+
     return 0;
 }
 
+sub random_key {
+    my $href = shift;
+    my @keys = keys %{$href};
+    my $numkeys = scalar @keys;
+    my $k = rand $numkeys;
+    return $keys[$k];
+}
+
+# TODO: parameterize $depth
+my $depth = 2;
+my %map = ();
+
+my @buf = ();
+
+# side effect: puts stuff into global %map
+sub handle_word {
+    my $word = shift;
+        if (exclude_line($word)) {
+            return;
+        }
+    if (scalar(@buf) == $depth) {
+        my $key = join " ", @buf;
+        $map{$key}{$word} += 1;
+        shift @buf;
+    } 
+
+    push (@buf, $word);
+}
+
+
 sub handle_sentence {
     my $sentence = shift;
-    print "hs: $sentence\n";
+    my @words = split /\s/, $sentence;
+    foreach my $w (@words) {
+        handle_word $w;
+    }
 }
 
+
+sub handle_sentences {
+    my $sref = shift;
+    my @sentences = @{$sref};
+    foreach my $s (@sentences) {
+        handle_sentence $s;
+    }
+}
+
+# TODO:
 # blank lines and periods are full stops so clear input buffer
-my @group = ();
+#my @group = ();
 
-my $sentence = "";
-my @sentences = ();
-while (my $line = <>) {
+sub get_sentences {
+    my $sentence = "";
+    my @sentences = ();
+    while (my $line = <>) {
+    
+        chomp($line);
+        #$line =~ s/[_"'\-'"\$#@~!&*()\[\];,:?^`\\\/]+//g;
+        # better to leave some punctuation
+        $line =~ s/[_\-\$#@~*()\[\]^`\\\/]+//g;
+        $line = lc($line);
+    
+        if (exclude_line($line)) {
+            next;
+        }
+    
+        # split into sentences on period
+        push (@sentences, split /\./,$line);
+    }
+    return \@sentences;
+}
 
-    #if ($line =~ /^\s+$/) {
-    #    print "blank line!\n";
-    #}
+sub make_sentences {
+    my $href = shift;
+    my $num = shift;
+    my $ret = "";
 
-    # remove commas etc
-    chomp($line);
-    $line =~ s/[\$#@~!&*()\[\];,:?^`\\\/]+//g;
-    $line = lc($line);
+    my $key = random_key $href;
 
-    if (exclude_line($line)) {
-        next;
+    $ret = $key . " ";
+
+    while ($num > 0) {
+        # this key manipulation could be improved
+        my $subkey = random_key $href->{$key};
+        $ret .= $subkey . " ";
+        @keyarr = split /\s/,$key;
+        shift @keyarr;
+        push @keyarr,$subkey;
+        $key = join " ",@keyarr;
+        $num--;
     }
 
-    print "$line\n";
-
-
-    # split into sentences on period
-    @sentences = (@sentences, split ".",$line);
-    my $sentence = pop(@sentences);
-    handle_sentence($sentence);
+    return $ret;
 }
+
+my $sentencesref = get_sentences();
+handle_sentences($sentencesref);
+
+# TODO: parameterize this stuff:
+#dump_map(\%map);
+$out = make_sentences(\%map, 300);
+print "$out\n";
